dd7818ec9357d912d837c690fc6802fd6a605d8b
Wilson-Flores <WillyFlores173@gmail.com>
2023-06-05 22:01:33 -0400

    finished adding the rest of Platform functions.

--- Platform.cpp
+++ Platform.cpp
@@ -48,3 +48,97 @@ Platform::Platform(char const* title, int windowWidth, int windowHeight, int tex
     // unbinds the currently bound texture. ID 0 represents the default texture
     glBindTexture(GL_TEXTURE_2D, 0);
 }
+
+
+Platform::~Platform(){
+
+    // destroy the texture object
+    SDL_DestroyTexture(texture);
+
+    // destroy renderer object
+    SDL_DestroyRenderer(renderer);
+
+    // destroy window object
+    SDL_DestroyWindow(window);
+
+    // shut down the SDL library, cleans up SDL subsystem
+    SDL_Quit();
+}
+
+
+void Platform::update(void const* buffer, int pitch){
+
+    // updates the texture with the pixel data, upload pixel data to GPU texture memory
+    SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+
+    // clears the rendering target
+    SDL_RenderClear(renderer);
+
+    // copies the entire 'texture' to the rendering target window associated wiht the 'renderer'
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+
+    // updates the window/screen with the rendered content, swaps the front and back buffers
+    SDL_RenderPresent(renderer);
+}
+
+
+bool Platform::ProcessInput(uint8_t* keys){
+    bool quit = false;
+
+    // a structure that represents an event that ouccured in the program
+    SDL_Event event;
+
+    // processing user input events and updating the 'keys' array 
+    while(SDL_PollEvent(&event)){
+        switch (event.type){
+            case SDL_QUIT:
+	            quit = true;
+	            break;
+
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym){
+                    case SDLK_ESCAPE: quit = true; break;
+					case SDLK_x: keys[0] = 1; break;
+					case SDLK_1: keys[1] = 1; break;
+					case SDLK_2: keys[2] = 1; break;
+					case SDLK_3: keys[3] = 1; break;
+					case SDLK_q: keys[4] = 1; break;
+					case SDLK_w: keys[5] = 1; break;
+					case SDLK_e: keys[6] = 1; break;
+					case SDLK_a: keys[7] = 1; break;
+					case SDLK_s: keys[8] = 1; break;
+					case SDLK_d: keys[9] = 1; break;
+					case SDLK_z: keys[0xA] = 1; break;
+					case SDLK_c: keys[0xB] = 1; break;
+					case SDLK_4: keys[0xC] = 1; break;
+					case SDLK_r: keys[0xD] = 1; break;
+					case SDLK_f: keys[0xE] = 1; break;
+					case SDLK_v: keys[0xF] = 1; break;
+                }
+                break;
+
+            case SDL_KEYUP:
+                switch (event.key.keysym.sym){
+ 					case SDLK_x: keys[0] = 0; break;
+					case SDLK_1: keys[1] = 0; break;
+					case SDLK_2: keys[2] = 0; break;
+					case SDLK_3: keys[3] = 0; break;
+					case SDLK_q: keys[4] = 0; break;
+					case SDLK_w: keys[5] = 0; break;
+					case SDLK_e: keys[6] = 0; break;
+					case SDLK_a: keys[7] = 0; break;
+					case SDLK_s: keys[8] = 0; break;
+					case SDLK_d: keys[9] = 0; break;
+					case SDLK_z: keys[0xA] = 0; break;
+					case SDLK_c: keys[0xB] = 0; break;
+					case SDLK_4: keys[0xC] = 0; break;
+					case SDLK_r: keys[0xD] = 0; break;
+					case SDLK_f: keys[0xE] = 0; break;
+					case SDLK_v: keys[0xF] = 0; break;                   
+                }
+                break;
+        }
+    }
+}
+
+
